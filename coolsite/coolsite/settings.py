b87863fb7f8581e200a8990e937f0c16d8eae7a0
suhanoves <suhanoves@gmail.com>
2021-04-27 17:20:30 +0300

    add views archive, handler404 and URL patterns for them

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -13,10 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-import women.apps
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -25,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t4*ggv1upm^0cdc)6hple@!dby#35=*(+)decyhgp22o^pi#1d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -72,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'coolsite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -82,7 +78,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -102,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -115,7 +109,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
@@ -7,5 +7,19 @@ def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse('Страница приложения Women')
 
 
-def categories(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('<h1>Статьи по категориям</h1>')
+def categories(request: HttpRequest, catid: int) -> HttpResponse:
+    if request.POST:
+        print(request.POST)
+
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+
+
+def archive(request: HttpRequest, year: str) -> HttpResponse:
+    if int(year) > 2020:
+        raise Http404()
+
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+
+def pageNotFound(request: HttpRequest, exception) -> HttpResponseNotFound:
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
