bd4de1aec7a4584d1f97ebaae68ad2ee49cc138c
suhanoves <suhanoves@gmail.com>
2021-04-28 08:24:35 +0300

    add new views: addpage, contact, login, show_post. Add proper links

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -5,6 +5,8 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
-    path('cats/<int:catid>/', categories),
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
 ]
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,34 +1,45 @@
-from django.http import HttpRequest, HttpResponse, HttpResponseNotFound, Http404
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
-from women.models import *
+from .models import *
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+        ]
 
 
-# Create your views here.
-def index(request: HttpRequest) -> HttpResponse:
+def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+
+    return render(request, 'women/index.html', context=context)
 
 
-def about(request: HttpRequest) -> HttpResponse:
-    return render(request, 'women/index.html', {'menu': menu, 'title': 'О сайте'})
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 
-def categories(request: HttpRequest, catid: int) -> HttpResponse:
-    if request.POST:
-        print(request.POST)
-
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+def addpage(request):
+    return HttpResponse("Добавление статьи")
 
 
-def archive(request: HttpRequest, year: str) -> HttpResponse:
-    if int(year) > 2020:
-        raise Http404()
-
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+def contact(request):
+    return HttpResponse("Обратная связь")
 
 
-def pageNotFound(request: HttpRequest, exception) -> HttpResponseNotFound:
+def login(request):
+    return HttpResponse("Авторизация")
+
+
+def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
